8122e3a7e85a6b9b19097bda1d470bf8ae45dcd5
zhaoyi <lightseeker@qq.com>
2024-04-05 17:57:04 +0800

    引入redisTemplate

---
+++ trd/trd-redistemplate/src/main/java/com/backend/trd/redistemplate/Config.java
@@ -0,0 +1,25 @@
+package com.backend.trd.redistemplate;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class Config {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        // 配置redisTemplate
+        RedisTemplate <String, Object> redisTemplate = new RedisTemplate <> ( );
+
+        redisTemplate.setKeySerializer ( new StringRedisSerializer( ) );
+        redisTemplate.setValueSerializer ( new JdkSerializationRedisSerializer( ) );
+        redisTemplate.setHashKeySerializer ( new StringRedisSerializer ( ) );
+        redisTemplate.setHashValueSerializer ( new StringRedisSerializer ( ) );
+
+        redisTemplate.setConnectionFactory ( connectionFactory );
+        return redisTemplate;
+    }
+}
