11e16a711d8998132415f1fc57c8f40782b67717
zhaoyi <lightseeker@qq.com>
2024-04-10 14:08:09 +0800

    发回的response需要实现Serializable

--- system/system-basedata/src/main/java/com/backend/system/basedata/user/view/vo/LoginUser.java
+++ system/system-basedata/src/main/java/com/backend/system/basedata/user/view/vo/LoginUser.java
@@ -2,8 +2,10 @@ package com.backend.system.basedata.user.view.vo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class LoginUser {
+public class LoginUser implements Serializable {
     private String token;
     private String loginTime;
 }
